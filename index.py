4545f72b159b2b49ae5331d43cde51e550f10dd8
Klavs Kruzins <88055533+klavsinthehouse@users.noreply.github.com>
2023-07-04 14:25:36 +0300

    Initial coomit

--- index.py
+++ index.py
@@ -7,10 +7,28 @@ learn = load_learner('./appliancesModel.pkl')
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(true)
-    image_url = data['image_url']
+    image_url = data['sessionInfo']['parameters']['image_url']
     img_data = requests.get(image_url).content
     img = PILImage.create(img_data)
     prediction = learn.predict(img)[0]
+    response = {
+            'fulfillmentMessages': [
+                {
+                    'text': {
+                        'text': ['Your response message']
+                    }
+                }
+            ],
+            'outputContexts': [
+                {
+                    'name': request['session'] + '/contexts/your_context_name',
+                    'lifespanCount': 5,
+                    'parameters': {
+                        'appliance': prediction
+                    }
+                }
+            ]
+        }
     return jsonify({"prediction": str(prediction)})
 
 if __name__ == '__main__':
